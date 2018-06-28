d53704144ba336ea8dce81de3b82ae5854c20554
Arsenii Bylym <arseniibylym@gmail.com>
2018-06-28 20:12:50 +0300

    Gallery animations

--- src/components/Gallery/Project/Picture/Picture.js
+++ src/components/Gallery/Project/Picture/Picture.js
@@ -2,15 +2,17 @@ import React from 'react';
 import './Picture.css';
 
 export default function Picture (props) {
-	console.log(props);
+	
 	const backgroundUrl = `url(${props.url})`;
-	console.log(backgroundUrl);
+	let classList = 'Picture';
+	if (props.pos == 0) {
+		classList += ' current';
+	}
 
 	return (
-		<div className='Picture' >
+		<div className={classList}>
 			<div className='Picture__info'>
-				<div><pre>Hole   60x80          acrylic on canvas   2017</pre></div>
-				
+				<div><p>{props.info}</p></div>
 			</div>
 			<div className='Picture__img' style={{backgroundImage: backgroundUrl}} />
 		</div>
--- src/components/Gallery/Project/Project.js
+++ src/components/Gallery/Project/Project.js
@@ -7,36 +7,66 @@ export default class Project extends Component {
 		super(props);
 		this.state = {
 			url: './img/Gallery/GridProject/img1.jpg',
-			// pictures: [
-			// 	{
-			// 		url: './img/Gallery/GridProject/img1.jpg',
-			// 		info: 'Hole, 60x80, acrylic on canvas, 2017' 
-			// 	},
-			// 	{
-			// 		url: './img/Gallery/GridProject/img2.jpg',
-			// 		info: 'Hole, 60x80, acrylic on canvas, 2017' 
-			// 	},
-			// 	{
-			// 		url: './img/Gallery/GridProject/img3.jpg',
-			// 		info: 'Hole, 60x80, acrylic on canvas, 2017' 
-			// 	},
-			// 	{
-			// 		url: './img/Gallery/GridProject/img4.jpg',
-			// 		info: 'Hole, 60x80, acrylic on canvas, 2017' 
-			// 	},
-			// ]
+			pictures: [
+				{
+					url: './img/Gallery/GridProject/img1.jpg',
+					info: 'Horizon 100x110 acrylic on canvas 2018' 
+				},
+				{
+					url: './img/Gallery/GridProject/img2.jpg',
+					info: 'Northern Lights 100x110 acrylic on canvas 2018' 
+				},
+				{
+					url: './img/Gallery/GridProject/img3.jpg',
+					info: 'Hole 60x80 acrylic on canvas 2017' 
+				},
+				{
+					url: './img/Gallery/GridProject/img4.jpg',
+					info: 'Frame 60x80 acrylic on canvas 2017' 
+				},
+			],
+			currentPictureIndex: 0
 		};
 	}
 
+	showNext = () => {
+		let images = [...document.getElementsByClassName('Picture')];
+
+		if(this.state.currentPictureIndex == images.length - 1) return;
+
+		images[this.state.currentPictureIndex].classList.remove("current");
+		images[this.state.currentPictureIndex].classList.add("prev");
+		images[this.state.currentPictureIndex + 1].classList.remove("next");
+		images[this.state.currentPictureIndex + 1].classList.add("current");
+		this.setState((prevState) => {
+			return {currentPictureIndex: prevState.currentPictureIndex + 1}
+		})
+	
+	};
+
+	showPrev = () => {
+		if(this.state.currentPictureIndex == 0) return;
+
+		let images = [...document.getElementsByClassName('Picture')];
+
+		images[this.state.currentPictureIndex].classList.remove("current");
+		images[this.state.currentPictureIndex].classList.add("next");
+		images[this.state.currentPictureIndex - 1].classList.add("current");
+		images[this.state.currentPictureIndex - 1].classList.remove("prev");
+		this.setState((prevState) => {
+			return {currentPictureIndex: prevState.currentPictureIndex - 1}
+		})
+	};
+
 	render() {
-		// let images = null;
-		// if (this.state.pictures) {
-		// 	images = this.state.pictures.map((img, i, arr) => {
-		// 		return (
-		// 			<Picture key={img.url} url={img.url} info={img.info} pos={i}/>	
-		// 		)
-		// 	})
-		// }
+		let images = null;
+		if (this.state.pictures) {
+			images = this.state.pictures.map((img, i) => {
+				return (
+					<Picture key={img.url} url={img.url} info={img.info} pos={i}/>	
+				)
+			})
+		}
 
 		return(
 			<div className='Project'>
@@ -45,11 +75,11 @@ export default class Project extends Component {
 					<p>{this.props.projectName}</p>
 				</div>
 				<div className='Project__wrapper'>
-					<Picture url={this.state.url} />
+					{images}
 					<div className='Project__controls'> 
-						<div className='Project__controls--prev'>Prev</div>
-						<div className='Project__controls--counter'><pre>1/3</pre></div>
-						<div className='Project__controls--next'>Next</div>
+						<div className='Project__controls--prev' onClick={this.showPrev}>Prev</div>
+						<div className='Project__controls--counter'><pre>{this.state.currentPictureIndex + 1}/{this.state.pictures.length}</pre></div>
+						<div className='Project__controls--next' onClick={this.showNext}>Next</div>
 						<div className='Project__controls--line left' />
 						<div className='Project__controls--line right' />
 					</div>
