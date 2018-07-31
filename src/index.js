290f9845dd614037e42253931a819286360b5202
Arsenii Bylym <arseniibylym@gmail.com>
2018-07-31 09:29:43 +0300

    Add LocalStorage for cache current firebase database

--- src/index.js
+++ src/index.js
@@ -24,57 +24,49 @@ window.addEventListener('load',()=>{
 })
 
 let promiseImg = new Promise((resolve) => {
-	console.log(window.navigator.onLine)
 	let img = document.createElement('img');
 	
 	img.onload = function () {
 		resolve();
 	}
-	img.src = "/art-gallery/static/media/img1_rsz.99d37f4f.jpg";
+	img.src = "/static/media/img1_rsz.99d37f4f.jpg";
 })
 
 let promiseData = new Promise((resolve) => {
 
 	if(window.navigator.onLine === false) {
-		window.GLOBAL_DATA = defaultData();
+		window.GLOBAL_DATA = defaultData(JSON.parse(localStorage.getItem('database')));
 		resolve();
 		return;
 	}
-	console.log('online')
 
 	window.GLOBAL_DATA = {};
 
 	firebaseDB.ref('/commonData').once('value')
-			.then((snapshot) => {
+	.then((snapshot) => {
+		localStorage.setItem('database', JSON.stringify(snapshot.val()))
 
-	let eventsArr = [];
-	snapshot.child('events').forEach((event) => {
-		eventsArr.push(event.val());
-	})
-
-	window.GLOBAL_DATA.EVENTS = eventsArr.reverse();
-
-	
-	let projectsArr = {};
-	snapshot.child('projects').forEach((project) => {
-		let projectArr = [];
-		project.forEach((item) =>{
-			projectArr.push(item.val());
+		let eventsArr = [];
+		snapshot.child('events').forEach((event) => {
+			eventsArr.push(event.val());
 		})
-		projectsArr[project.key] = projectArr;
-	});
+		window.GLOBAL_DATA.EVENTS = eventsArr;
+		
+		let projectsArr = {};
+		snapshot.child('projects').forEach((project) => {
+			let projectArr = [];
+			project.forEach((item) =>{
+				projectArr.push(item.val());
+			})
+			projectsArr[project.key] = projectArr;
+		});
+		window.GLOBAL_DATA.PROJECTS = projectsArr;
 
-	window.GLOBAL_DATA.PROJECTS = projectsArr;
-
-	let projectsList = [];
-	snapshot.child('projectsList').forEach((item) => {
-		projectsList.push(item.val());
-	})
-	window.GLOBAL_DATA.PROJECT_LIST = projectsList;
-
-		setTimeout(()=>{
-			resolve();
-		}, 3000)
+		let projectsList = [];
+		snapshot.child('projectsList').forEach((item) => {
+			projectsList.push(item.val());
+		})
+		window.GLOBAL_DATA.PROJECT_LIST = projectsList;
 
 		resolve();
 	})
